308f933b0bd42b45b914bce77a21a9b00055bd19
delta student <jeevandsouza1241@gmail.com>
2023-08-30 13:17:30 -0400

    first commit

---
+++ oops.java
@@ -0,0 +1,44 @@
+package jeevan;
+
+
+import java.util.Scanner;  
+
+
+
+
+
+
+
+class calculator{
+	
+	
+	public int test(int num1 , int num2) {
+		return num1+num2;
+	
+	}
+	
+	
+	
+	
+}
+	
+public class Launch {
+
+	public static void main(String[] args) {
+		
+		 Scanner myObj = new Scanner(System.in);
+		 
+		    System.out.println("Enter username");
+
+		    String userName = myObj.nextLine();  // Read user input
+		
+			System.out.println(userName);
+			
+			calculator jeevan1	= new calculator();
+		
+			
+			
+	  }
+	
+
+}
