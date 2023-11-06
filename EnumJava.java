a4d726ce5323aea121b8f2cbcd31488280bcabf5
delta student <jeevandsouza1241@gmail.com>
2023-11-06 01:55:31 -0500

    enum in Java

---
+++ EnumJava.java
@@ -0,0 +1,34 @@
+package jeevan;
+
+
+
+
+
+enum Result1
+{
+	PASS, FAIL, NR;
+}
+public class EnumJava 
+{
+
+	public static void main(String[] args) 
+	{
+//		int i=10;
+//		String str="JAVA";
+		
+		Result1 res=Result1.PASS;
+		
+		
+		
+		switch(res)
+		{
+		 	case PASS: System.out.println("Passed the exam");
+		 	break;
+		 	case FAIL: System.out.println("Failed!");
+		 	break;
+		 	case NR: System.out.println("No result");
+		
+		}
+	}
+
+}
